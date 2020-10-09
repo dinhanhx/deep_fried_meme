1b1fd07df1298a47c1cd8f5e86185ccbdab6501d
Dinh Anh <dinhanhx@gmail.com>
2020-10-09 10:03:58 +0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+# The versioning scheme of this package is undefined
+setup(name='deep_fried_meme',
+        version='69.420',
+        description='The library to fry memes deeply',
+        author='Dinh Anh Vu',
+        author_email='dinhanhx@gmail.com',
+        license='MIT',
+        packages=['deep_fried_meme'])
